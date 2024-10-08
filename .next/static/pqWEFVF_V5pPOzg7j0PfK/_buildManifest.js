fb1aaea503ad92fa0c0b6de604ef8a2a830664e0
brennankapollock <brennankapollock@gmail.com>
2024-10-07 17:25:53 -0700

    redo

---
+++ .next/static/pqWEFVF_V5pPOzg7j0PfK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/861-7f7660ed19ddd4d5.js","static/chunks/pages/index-0c28b667b445ae75.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
