e216476f4e206c646cc233461355f25da6252a79
brennankapollock <brennankapollock@gmail.com>
2024-10-17 08:16:14 -0700

    changed copy

--- src/components/service/ServiceItems.js
+++ src/components/service/ServiceItems.js
@@ -6,8 +6,8 @@ const services = [
     id: 1,
     name: 'Details',
     text: [
-      'Our retreat will begin around 5pm the 1st, but you are welcome to arrive anytime after 4pm. The retreat will end at 11am on the 3rd, and we ask that all participants be there until that time.',
-      'Every activity - group or solo - is not about adherence to aschedule or pleasing others, but we want to create an opportunity for you to listen to yourself, learning from your own heart and others around you.',
+      'Our retreat will begin around 5pm the 13th, but you are welcome to arrive anytime after 4pm. The retreat will end at 11am on the 15th, and we ask that all participants be there until that time.',
+      'Every activity - group or solo - is not about adherence to a schedule or pleasing others, but we want to create an opportunity for you to listen to yourself, learning from your own heart and others around you.',
     ],
   },
   {
@@ -29,8 +29,8 @@ const services = [
     id: 4,
     name: 'Retreat Prep',
     text: [
-      ' A brief questionnaire will be sent to you upon registration, regarding how you can best prepare for the retreat and how we can get to know you better.',
-      'This email will also provide a very basic outline of what will happen over the course of the retreat.',
+      ' Upon registration, you’ll receive a welcome email that will provide details on how you can best prepare for the retreat. It will also include a very basic outline of what will happen over the course of the retreat.',
+      'We’ll also ask you to provide any information about yourself that you feel you want to share so we can get to know you better and serve you well at the retreat.',
     ],
     image: 'assets/img/news/4.jpg',
   },
@@ -39,15 +39,31 @@ const services = [
     name: 'Cost',
     text: [
       'Please let us know if making a single payment is an obstacle for you as we also have scholarships available.',
-      'The cost of the retreat is $1100.00',
+      'The cost of the retreat is $1000.00',
     ],
   },
   {
     id: 6,
+    name: 'Accommodations & Food',
+    text: [
+      'We will have limited on site accommodations for those of you who are interested. This will be a "first come first serve” basis, so please let us know as soon as you can if you would like a bed on site.',
+      'If you request a room in your email and there is space still available we will let you know. Here is the cost breakdown for an on-site room for the entirety of the retreat:',
+      '- $300 for a private room (2 beds available)',
+      '- $200 for a shared room (4 beds available)',
+      '- $50 for barn bed further away from the house (2 beds available)',
+      'If you requested an on site accommodations but there is no room left, or you prefer to arrange your own,  there will be a facebook group that you can join once you have paid for your ticket or a deposit.',
+      'There you can coordinate accommodation or travel with others coming to the retreat.',
+      'Meals included are dinner the first night, and the breakfasts the following two days. There will be some fridge space for you to bring food for other meals, and also long breaks for you to get or prepare your own meals.',
+      'We will have a wide variety of foods for the meals for those who are vegan, gluten free, etc.',
+    ],
+  },
+  {
+    id: 7,
     name: 'Want to Join?',
     text: [
       'Space is limited to 27 participants, if you would like to come, please send an email to team@sacredfeminine.co with your first and last name.',
-      'These e-mails will be answered on a first come first serve basis. If you decide to join, we will provide a link for you to make an online payment for the retreat.',
+      'If you want to request on site accommodation (see accommodation section for details) please include which room type you prefer in your email as well.',
+      'These e-mails will be answered on a first come first serve basis. If you decide to join, we will provide a link for you to make an online payment for the retreat (and your room if applicable).',
     ],
   },
 ];
@@ -86,6 +102,11 @@ const ServiceItems = () => {
               )}
               {service.id == 6 && (
                 <span className="number inline-block mb-[25px] relative w-[60px] h-[60px] leading-[60px] text-center rounded-full bg-sf_yellow font-bold text-black transition-all duration-300">
+                  {service.id <= 9 ? `0${service.id}` : service.id}
+                </span>
+              )}
+              {service.id == 7 && (
+                <span className="number inline-block mb-[25px] relative w-[60px] h-[60px] leading-[60px] text-center rounded-full bg-chefchaouen_blue font-bold text-black transition-all duration-300">
                   {service.id <= 9 ? `0${service.id}` : service.id}
                 </span>
               )}
