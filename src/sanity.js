6b686a50bd84e9e9436a48fd176cc6ae47c877a5
brennankapollock <brennankapollock@gmail.com>
2024-10-21 18:37:14 -0700

    added favicon and analytics

--- src/sanity.js
+++ src/sanity.js
@@ -1,7 +1,7 @@
 import { createClient } from '@sanity/client';
 
-export const client = createClient({
-  projectId: 'n031luuh',
-  dataset: 'production',
-  useCdn: false, // Set to `false` for fresh data
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  useCdn: process.env.NEXT_PUBLIC_SANITY_USE_CDN === 'true',
 });
