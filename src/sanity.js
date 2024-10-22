cc2ae02bc285aa8024699e9c4a8fa225f23ab5fa
brennankapollock <brennankapollock@gmail.com>
2024-10-21 18:53:43 -0700

    added api version

--- src/sanity.js
+++ src/sanity.js
@@ -3,5 +3,6 @@ import { createClient } from '@sanity/client';
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: '2021-10-21',
   useCdn: process.env.NEXT_PUBLIC_SANITY_USE_CDN === 'true',
 });
