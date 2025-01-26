1c087248505f1126c18828f1325b883091466159
Brennan K.A. Pollock <brennankapollock@gmail.com>
2025-01-25 18:54:20 -0800

    Update Checkout.js

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -145,7 +145,7 @@ export default function Checkout() {
                 href="mailto:support@sacredfeminine.com"
                 className="text-[#b5a397] hover:text-[#927f73] underline"
               >
-                team@sacredfeminine.com
+                team@sacredfeminine.co
               </a>
             </p>
           </div>
