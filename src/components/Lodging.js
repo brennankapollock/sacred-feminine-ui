147000bd8729e8acb3821e0cf028ae527210ede2
Brennan K.A. Pollock <brennankapollock@gmail.com>
2025-01-25 18:47:33 -0800

    Update Lodging.js

--- src/components/Lodging.js
+++ src/components/Lodging.js
@@ -196,7 +196,7 @@ export default function Lodging() {
                 href="mailto:support@sacredfeminine.com"
                 className="text-[#b5a397] hover:text-[#927f73] underline"
               >
-                support@sacredfeminine.com
+                team@sacredfeminine.com
               </a>
             </p>
           </div>
